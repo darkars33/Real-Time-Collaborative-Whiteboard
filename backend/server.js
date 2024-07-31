c447005493889a99d3e3d1bb5ba72bf6487f5866
darkars33 <darshangarad18@gmail.com>
2024-08-01 01:10:29 +0530

    add response image logic

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,45 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-          res.send('Hello World!');
-})
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(port, () => {
-          console.log(`app listening at http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+let roomIdGlobal, imgURLGlobal;
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("userJoined", (data) => {
+    const { name, roomId, userId, host, presenter } = data;
+    roomIdGlobal = roomId;
+    socket.join(roomId);
+    socket.emit("userIsJoined", { success: true });
+    socket.broadcast.to(roomId).emit("whiteBoardDataResponse", {
+      imgURL: imgURLGlobal,
+    })
+    console.log(`${name} joined room ${roomId}`);
+  });
+
+  socket.on("stageImage", (data) => {
+    imgURLGlobal = data;
+    socket.broadcast.to(roomIdGlobal).emit("whiteBoardDataResponse", {
+      imgURL: imgURLGlobal,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
