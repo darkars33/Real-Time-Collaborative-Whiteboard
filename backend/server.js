d83056afc358ea41f2af6e648d461a4e519d8f15
darkars33 <darshangarad18@gmail.com>
2024-08-02 10:48:07 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: "*",
+}))
 
 const server = http.createServer(app);
 const io = new Server(server);
